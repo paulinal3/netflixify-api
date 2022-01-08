56818a50c83653ac73e95720ccebf79702f72ec6
paulinal3 <paulinal3@outlook.com>
2022-01-07 20:56:05 -0500

    creating video collection

--- app/models/video.js
+++ app/models/video.js
@@ -1,24 +1,53 @@
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-const exampleSchema = new mongoose.Schema(
+const videoSchema = new mongoose.Schema(
 	{
 		title: {
 			type: String,
 			required: true,
 		},
-		text: {
+		synopsis: {
 			type: String,
 			required: true,
 		},
-		owner: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'User',
+		netflixid: {
+			type: Number,
 			required: true,
 		},
+		image: {
+			type: String,
+			required: true
+		},
+		genre: {
+			type: Array,
+			required: true
+		},
+		type: {
+			type: String,
+			required: true
+		},
+		runtime: {
+			type: String,
+			required: true
+		},
+		released: {
+			type: Number,
+			required: true
+		},
+		rating: {
+			type: D
+		},
+		imbdid: {
+			type: String,
+			required: true
+		},
+		watched: Boolean,
+		notes: <String></String>
 	},
 	{
 		timestamps: true,
 	}
 )
 
-module.exports = mongoose.model('Example', exampleSchema)
+module.exports = mongoose.model('Video', videoSchema)
